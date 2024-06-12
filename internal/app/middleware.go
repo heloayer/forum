ec4f3c1a47e2d314568efb1321136f267ec039bf
Ansar Issabekov <103923930+heloayer@users.noreply.github.com>
2024-06-12 18:25:10 +0500

    Update middleware.go

--- internal/app/middleware.go
+++ internal/app/middleware.go
@@ -8,13 +8,13 @@ import (
 )
 
 var (
-	WelcomeCookieOnPaths = make(map[string]struct{}, 10) // карта с ключами путей unauth. users со значением пустой структуры
-	HomeCookieOnPaths    = make(map[string]struct{}, 10) // карта с ключами путей auth. users со значением пустой структуры
+	WelcomeCookieOnPaths = make(map[string]struct{}, 10) 
+	HomeCookieOnPaths    = make(map[string]struct{}, 10)
 )
 
 func AddWelcomeCookieCheckOnPaths(paths ...string) {
 	for _, path := range paths {
-		WelcomeCookieOnPaths[path] = struct{}{} // struct{} просто тип, чтобы указать что он пусть нужно {}{}
+		WelcomeCookieOnPaths[path] = struct{}{} 
 	}
 }
 
@@ -26,19 +26,18 @@ func AddHomeCookieCheckOnPaths(paths ...string) {
 
 func (app *App) WelcomeMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if _, ok := WelcomeCookieOnPaths[r.URL.Path]; !ok { // проверяем имеется ли путь в карте, ; ok??
+		if _, ok := WelcomeCookieOnPaths[r.URL.Path]; !ok {
 			pkg.ErrorHandler(w, http.StatusNotFound)
 			return
-		} else {        // если путь действителен
-			c, err := r.Cookie("session_token") // присваиваем токен (session token), указанный в запросе через метод Cookie
-			if err == http.ErrNoCookie {        // если токена нет
-				next.ServeHTTP(w, r) // то возвращаем реализуем request, response обработчика и выходим из функции
+		} else {       
+			c, err := r.Cookie("session_token") 
+			if err == http.ErrNoCookie {      
+				next.ServeHTTP(w, r) 
 				return
 			}
-			sessionFromDb, err := app.sessionService.GetSessionByToken(c.Value)  // операция ля поля интерфейса app (repository/session)
-			             // вызываем GetSessionByToken для знач. Value ("session token") структуры Cookie
+			sessionFromDb, err := app.sessionService.GetSessionByToken(c.Value)  
 			if err != nil {
-				next.ServeHTTP(w, r)   // если все ок инициализируем оригинальный обработчик
+				next.ServeHTTP(w, r) 
 				return
 			}
 			if sessionFromDb.Expiry.Before(time.Now()) {

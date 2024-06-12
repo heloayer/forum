c9afdb4d4232e19f22651bcc63b1c95a34ae4af4
Ansar Issabekov <103923930+heloayer@users.noreply.github.com>
2024-06-12 18:27:03 +0500

    Update data_access_object.go

--- internal/repository/data_access_object.go
+++ internal/repository/data_access_object.go
@@ -2,36 +2,36 @@ package repository
 
 import "database/sql"
 
-type DAO interface { // предост. операции с данными, не раскрывая детали БД. Имеет нужные методы для удобного доступа к данным
-	NewUserQuery() UserQuery      // запрос нового юзера
-	NewSessionQuery() SessionQuery    // запрос новой сессии
-	NewPostQuery() PostQuery     // запрос нового поста
+type DAO interface { 
+	NewUserQuery() UserQuery     
+	NewSessionQuery() SessionQuery   
+	NewPostQuery() PostQuery    
 }
 
-type dao struct { // dao перенимает данные с БД
+type dao struct {
 	db *sql.DB       
 }
 
-func NewDao(db *sql.DB) DAO { // возвращаем указатель на структуру dao с вложенным db, теперь мы можем получать опред. данные с пом. методов интерфейса DAO
-	return &dao{ // возвращаем указатель на структуру
+func NewDao(db *sql.DB) DAO { 
+	return &dao{ 
 		db: db,
 	}
 }
 
 func (d dao) NewUserQuery() UserQuery {
-	return &userQuery{                   // repository/user
-		db: d.db,         // assign data from dao to user Query struct
+	return &userQuery{                 
+		db: d.db,        
 	}
 }
 
 func (d dao) NewSessionQuery() SessionQuery {
-	return &sessionQuery{            // repository/session
+	return &sessionQuery{           
 		db: d.db,           
 	}
 }
 
 func (d dao) NewPostQuery() PostQuery {
-	return &postQuery{              // repository/post
-		db: d.db,             // assign data from dao to SessionQuery struct
+	return &postQuery{   
+		db: d.db,           
 	}
 }
